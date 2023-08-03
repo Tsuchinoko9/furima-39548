3b5f63660c45c3f212a9b212e7e1a753ab58029f
Tsuchinoko9 <tdkyou9@gmail.com>
2023-08-03 17:14:47 +0900

    Rubocop実行により一部修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,16 +1,16 @@
 FactoryBot.define do
   factory :user do
     transient do
-      test_user {Gimei.name}
+      test_user { Gimei.name }
     end
-    nickname {Faker::Name.initials(number: 3)}
-    email {Faker::Internet.email}
-    password {Faker::Lorem.characters(number: (6..128), min_alpha: 1, min_numeric: 1)}
-    password_confirmation {password}
-    last_name {test_user.last.kanji}
-    first_name {test_user.first.kanji}
-    last_name_kana {test_user.last.katakana}
-    first_name_kana {test_user.first.katakana}
-    birth_date {Faker::Date.between(from: '1930-01-01', to: '2018-12-31')}
+    nickname { Faker::Name.initials(number: 3) }
+    email { Faker::Internet.email }
+    password { Faker::Lorem.characters(number: (6..128), min_alpha: 1, min_numeric: 1) }
+    password_confirmation { password }
+    last_name { test_user.last.kanji }
+    first_name { test_user.first.kanji }
+    last_name_kana { test_user.last.katakana }
+    first_name_kana { test_user.first.katakana }
+    birth_date { Faker::Date.between(from: '1930-01-01', to: '2018-12-31') }
   end
 end
