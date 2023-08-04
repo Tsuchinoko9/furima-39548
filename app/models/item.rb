56e2bc7fec1616c02b905e8d195d7bd86de242d8
Tsuchinoko9 <tdkyou9@gmail.com>
2023-08-04 14:02:34 +0900

    itemのバリデーションを設定
    アソシエーションで「belongs_to :user」を指定したことにより、「user_idが空ではないか」というバリデーションがデフォルトでかかるため、その旨のバリーションの記載は不要

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,20 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :name
+    validates :info
+    validates :category_id
+    validates :sales_status_id
+    validates :shipping_fee_status_id
+    validates :prefecture_id
+    validates :scheduled_delivery_id
+    validates :price
+  end
+
+  validates :price, numericality:{ only_integer: true, 
+                                   greater_than_or_equal_to: 300, 
+                                   less_than_or_equal_to: 9999999 },
+                    format: { with: /\A[0-9]+\z/ }
 end
