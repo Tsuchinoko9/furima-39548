87c4fb7ffcfb0057d752da85460b2001b95ba765
Tsuchinoko9 <tdkyou9@gmail.com>
2023-08-04 17:35:32 +0900

    itemのバリデーションを追加で設定
    ・itemsテーブルに紐づく画像ファイルが空では保存できないように設定
    ・ActiveHashを利用するモデルファイルのidを保存するカラムが、数値かつ「id:1以外」の値の場合に保存できるように設定
    ・ActiveHashを利用するモデルファイルのidを保存する際、「id:1」の値だった場合に出るエラーメッセージを編集
    ・一部コードのスペースを調整

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,8 +9,9 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :scheduled_delivery
 
-  
+
   with_options presence: true do
+    validates :image
     validates :name
     validates :info
     validates :category_id
@@ -21,8 +22,16 @@ class Item < ApplicationRecord
     validates :price
   end
 
-  validates :price, numericality:{ only_integer: true, 
-                                   greater_than_or_equal_to: 300, 
-                                   less_than_or_equal_to: 9999999 },
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id
+    validates :sales_status_id
+    validates :shipping_fee_status_id
+    validates :prefecture_id
+    validates :scheduled_delivery_id
+  end
+
+  validates :price, numericality: { only_integer: true, 
+                                    greater_than_or_equal_to: 300, 
+                                    less_than_or_equal_to: 9999999 },
                     format: { with: /\A[0-9]+\z/ }
 end
