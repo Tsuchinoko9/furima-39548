4fe9a8a738c8dbf9e27ccc778a54a419ebd92790
Tsuchinoko9 <tdkyou9@gmail.com>
2023-08-07 15:00:39 +0900

    商品出品の単体テストを実装

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,98 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品の出品' do
+    context '商品の出品ができる場合' do
+      it 'image・name・info・category_id・sales_status_id・shipping_fee_status_id・
+          prefecture_id・scheduled_delivery_id・priceが
+          適切な値で存在すれば商品情報をデータベースに保存して出品できる' do
+        expect(@item).to be_valid
+      end 
+    end
+
+    context '商品の出品ができない場合' do
+      it 'imageが空では出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+
+      it 'nameが空では出品できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+
+      it 'infoが空では出品できない' do
+        @item.info = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Info can't be blank")
+      end
+
+      it 'category_idが1(カテゴリーが空)では出品できない' do
+        @item.category_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+
+      it 'sales_status_idが1(商品の状態が空)では出品できない' do
+        @item.sales_status_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Sales status can't be blank")
+      end
+
+      it 'shipping_fee_status_idが1(配送料の負担が空)では出品できない' do
+        @item.shipping_fee_status_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping fee status can't be blank")
+      end
+
+      it 'prefecture_idが1(発送元の地域が空)では出品できない' do
+        @item.prefecture_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+
+      it 'scheduled_delivery_idが1(発送までの日数が空)では出品できない' do
+        @item.scheduled_delivery_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Scheduled delivery can't be blank")
+      end
+
+      it 'priceが空では出品できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+
+      it 'priceが300より小さい値では出品できない' do
+        @item.price = '299'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+
+      it 'priceが9999999より大きい値では出品できない' do
+        @item.price = '10000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+
+      it 'priceが半角数字以外では出品できない' do
+        @item.price = '７０００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+
+#      it 'userが紐付いていないと出品できない' do
+#        @item.user = nil
+#        @item.valid?
+#        binding.pry
+#        expect(@item.errors.full_messages).to include('User must exist')
+#      end
+    end
+  end
 end
