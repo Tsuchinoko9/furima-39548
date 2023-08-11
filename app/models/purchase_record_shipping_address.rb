6d1d8a19d0a8c6c310449fcbcd1478661f500623
Tsuchinoko9 <tdkyou9@gmail.com>
2023-08-11 15:09:48 +0900

    各テーブルにデータを保存する処理を設定

--- app/models/purchase_record_shipping_address.rb
+++ app/models/purchase_record_shipping_address.rb
@@ -3,13 +3,13 @@ class PurchaseRecordShippingAddress
   attr_accessor :item_id, :user_id,
                 :postal_code, :prefecture_id, :prefecture_id, :city, :addresses, :phone_number, :purchase_record_id
 
-  #purchase_recordsのバリデーション
+  # purchase_recordsのバリデーション
   with_options presence: true do
     validates :item_id
     validates :user_id
   end
 
-  #shipping_addressesのバリデーション
+  # shipping_addressesのバリデーション
   with_options presence: true do
     validates :postal_code,       format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
     validates :prefecture_id,     numericality: { other_than: 1, message: "can't be blank" } do
@@ -19,4 +19,12 @@ class PurchaseRecordShippingAddress
     validates :purchase_record_id
   end
 
+  def save
+    # purchase_recordsテーブルにデータを保存する処理
+    purchase_record = PurchaseRecord.create(item_id: item_id, user_id: user_id)
+    # shipping_addressesテーブルにデータを保存する処理
+    # purchase_record_idには、上の行で定義した変数purchase_recordのidと指定する
+    ShippingAddresses.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, 
+                             addresses: addresses, building: building, phone_number: phone_number, purchase_record_id: purchase_record.id)
+  end
 end
