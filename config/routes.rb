53d71343b9671d0e877fad6c5b33f52013687ed2
Tsuchinoko9 <tdkyou9@gmail.com>
2023-08-11 15:35:15 +0900

    purchase_recordsのcreateアクションのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :items do
-    resources :purchase_records, only: :index
+    resources :purchase_records, only: [:index, :create]
   end
   get '/items/:id/destroy', to: 'items#redirect_to_top', as: :redirect_to_top
 end
