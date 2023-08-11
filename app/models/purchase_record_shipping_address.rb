ff99cbb92e4f0f4b4ca1a58966b77f4bf691bcdc
Tsuchinoko9 <tdkyou9@gmail.com>
2023-08-11 16:55:07 +0900

    purchase_record_shipping_addressのモデルファイルを修正
    余計なdoを削除

--- app/models/purchase_record_shipping_address.rb
+++ app/models/purchase_record_shipping_address.rb
@@ -12,7 +12,7 @@ class PurchaseRecordShippingAddress
   # shipping_addressesのバリデーション
   with_options presence: true do
     validates :postal_code,       format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
-    validates :prefecture_id,     numericality: { other_than: 1, message: "can't be blank" } do
+    validates :prefecture_id,     numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :addresses
     validates :phone_number,      format: { with: /\A[0-9]{10}$|^[0-9]{11}\z/ }
