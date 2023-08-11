6fcdca72dca00a43444628737669987b637d8c43
Tsuchinoko9 <tdkyou9@gmail.com>
2023-08-11 14:42:13 +0900

    保存したい複数のテーブルのカラム名と各カラムに対するバリデーションを設定
    (備考)このクラスにアソシエーションを定義することはできないため、belongs_toによってitem_id、user_id、purchase_record_idに関してデータが空ではないかというバリデーションはできない。そのためその旨のバリデーションを設定する必要がある。

--- app/models/purchase_record_shipping_address.rb
+++ app/models/purchase_record_shipping_address.rb
@@ -1,4 +1,22 @@
 class PurchaseRecordShippingAddress
   include ActiveModel::Model
-  attr_accessor :hoge,:fuga
+  attr_accessor :item_id, :user_id,
+                :postal_code, :prefecture_id, :prefecture_id, :city, :addresses, :phone_number, :purchase_record_id
+
+  #purchase_recordsのバリデーション
+  with_options presence: true do
+    validates :item_id
+    validates :user_id
+  end
+
+  #shipping_addressesのバリデーション
+  with_options presence: true do
+    validates :postal_code,       format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
+    validates :prefecture_id,     numericality: { other_than: 1, message: "can't be blank" } do
+    validates :city
+    validates :addresses
+    validates :phone_number,      numericality: { only_integer: true, length: { in: 10..11 } }
+    validates :purchase_record_id
+  end
+
 end
