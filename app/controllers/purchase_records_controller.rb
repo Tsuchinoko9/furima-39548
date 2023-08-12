212bccf39ef2145a3fc1cd05f5be208fb25615f7
Tsuchinoko9 <tdkyou9@gmail.com>
2023-08-12 15:58:33 +0900

    purchase_recordsテーブルとshipping_addressesテーブルに入力フォームから情報が保存できるように設定
    ・Formオブジェクトを使用
    ・合わせてコントローラーファイル内の「purchase_record_shipping_addresses」表記を「purchase_record_shipping_address」に修正。

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,24 +1,30 @@
 class PurchaseRecordsController < ApplicationController
+  before_action :set_item_id, only: [:index, :create]
 
   def index
-    @purchase_record_shipping_addresses = PurchaseRecordShippingAddress.new
+    @purchase_record_shipping_address = PurchaseRecordShippingAddress.new
   end
 
   def create
-    @purchase_record_shipping_addresses = PurchaseRecordShippingAddress.new(purchase_record_params)
-    if @purchase_record_shipping_addresses.valid?
-      @purchase_record_shipping_addresses.save
+    @purchase_record_shipping_address = PurchaseRecordShippingAddress.new(purchase_record_params)
+    if @purchase_record_shipping_address.valid?
+      @purchase_record_shipping_address.save
       redirect_to root_path
     else
-      render :new, status: :unprocessable_entity
+      render :index, status: :unprocessable_entity
     end
   end
 
   private
 
   def purchase_record_params
-    params.require(:purchase_record_shipping_addresses)
-          .permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number)
-          .merge(user_id: current_user.id, items_id: @item.id)
+    # @item.idの値はcreateアクション前にset_item_idが実行されて取得される
+    params.require(:purchase_record_shipping_address)
+          .permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :purchase_record_id)
+          .merge(user_id: current_user.id, item_id: @item.id)
+  end
+
+  def set_item_id
+    @item = Item.find(params[:item_id])
   end
 end
--- app/models/purchase_record_shipping_address.rb
+++ app/models/purchase_record_shipping_address.rb
@@ -1,7 +1,7 @@
 class PurchaseRecordShippingAddress
   include ActiveModel::Model
   attr_accessor :item_id, :user_id,
-                :postal_code, :prefecture_id, :prefecture_id, :city, :addresses, :phone_number, :purchase_record_id
+                :postal_code, :prefecture_id, :prefecture_id, :city, :addresses, :building, :phone_number
 
   # purchase_recordsのバリデーション
   with_options presence: true do
@@ -16,15 +16,14 @@ class PurchaseRecordShippingAddress
     validates :city
     validates :addresses
     validates :phone_number,      format: { with: /\A[0-9]{10}$|^[0-9]{11}\z/ }
-    validates :purchase_record_id
   end
 
   def save
     # purchase_recordsテーブルにデータを保存する処理
     purchase_record = PurchaseRecord.create(item_id: item_id, user_id: user_id)
     # shipping_addressesテーブルにデータを保存する処理
-    # purchase_record_idには、上の行で定義した変数purchase_recordのidと指定する
-    ShippingAddresses.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, 
-                             addresses: addresses, building: building, phone_number: phone_number, purchase_record_id: purchase_record.id)
+    # purchase_record_idには、上の行で定義した変数purchase_recordのidを指定する
+    ShippingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, 
+                           addresses: addresses, building: building, phone_number: phone_number, purchase_record_id: purchase_record.id)
   end
 end
