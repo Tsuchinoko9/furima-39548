e2bc2cdbb9c245fcd60740632f927f49f9dd2ae3
Tsuchinoko9 <tdkyou9@gmail.com>
2023-08-15 12:58:51 +0900

    ログイン状態で自身が出品していない売却済み商品の商品購入ページへ遷移しようとすると、トップページに遷移するよう設定
    ・合わせてコントローラー内の記述をいくつか簡略化のため変更

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,11 +1,11 @@
 class PurchaseRecordsController < ApplicationController
-  before_action :authenticate_user!, only: [:index, :create]
-  before_action :set_item_id, only: [:index, :create]
-  before_action :user_check_move_to_top, only: [:index, :create]
- 
+  before_action :authenticate_user!
+  before_action :set_item_id
+  before_action :user_check_move_to_top
+  before_action :on_sale_check_move_to_top
 
   def index
-    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+    set_public_key
     @purchase_record_shipping_address = PurchaseRecordShippingAddress.new
   end
 
@@ -17,7 +17,7 @@ class PurchaseRecordsController < ApplicationController
       redirect_to root_path
     else
       # 入力エラーで遷移時にJavascript動作のため公開鍵の環境変数を定義
-      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+      set_public_key
       render :index, status: :unprocessable_entity
     end
   end
@@ -45,9 +45,21 @@ class PurchaseRecordsController < ApplicationController
   end
 
   def user_check_move_to_top
-    # @item.idの値は事前にset_item_idが実行されて取得される
-    return unless current_user.id == @item.user_id
+    # @itemは事前にset_item_idが実行されて定義される
+    return if current_user.id != @item.user_id
 
     redirect_to root_path
   end
+
+  def on_sale_check_move_to_top
+    # @itemは事前にset_item_idが実行されて取得される
+    return if PurchaseRecord.where(item_id: @item.id).none?
+
+    redirect_to root_path
+  end
+
+  def set_public_key
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+  end
+
 end
