da5313a70ca71f62de623229d8f39c0c61139800
Tsuchinoko9 <tdkyou9@gmail.com>
2023-08-15 11:45:08 +0900

    ログアウト状態で商品購入ページに遷移しようとすると、商品の販売状況に関わらずログインページに遷移するよう設定

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,4 +1,5 @@
 class PurchaseRecordsController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
   before_action :set_item_id, only: [:index, :create]
 
   def index
