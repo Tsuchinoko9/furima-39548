4aa72b19b83545eb884b33200f0f34540935f6a2
Tsuchinoko9 <tdkyou9@gmail.com>
2023-08-15 11:31:58 +0900

    クレジットカード決済機能を作成
    ・PAY.JPのオープンAPIを利用
    ・「payjp」と「gon」のGemをインストール
    ・商品詳細の「購入画面に進む」ボタンに「data:{turbo: false}」のオプションを指定(Javascriptの動作に影響を及ぼさない様にするため)
    (備考)環境変数を設定したら「その設定したターミナル」でサーバーの再起動が必要

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -2,15 +2,19 @@ class PurchaseRecordsController < ApplicationController
   before_action :set_item_id, only: [:index, :create]
 
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @purchase_record_shipping_address = PurchaseRecordShippingAddress.new
   end
 
   def create
     @purchase_record_shipping_address = PurchaseRecordShippingAddress.new(purchase_record_params)
     if @purchase_record_shipping_address.valid?
+      pay_item
       @purchase_record_shipping_address.save
       redirect_to root_path
     else
+      # 入力エラーで遷移時にJavascript動作のため公開鍵の環境変数を定義
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       render :index, status: :unprocessable_entity
     end
   end
@@ -21,10 +25,20 @@ class PurchaseRecordsController < ApplicationController
     # @item.idの値はcreateアクション前にset_item_idが実行されて取得される
     params.require(:purchase_record_shipping_address)
           .permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :purchase_record_id)
-          .merge(user_id: current_user.id, item_id: @item.id)
+          .merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
   end
 
   def set_item_id
     @item = Item.find(params[:item_id])
   end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"] 
+    Payjp::Charge.create(
+      amount: @item.price,                            # 商品の値段
+      card: @purchase_record_shipping_address.token,  # カードトークン
+      currency: 'jpy'                                 # 通貨の種類（日本円）
+    )
+  end
+
 end
--- spec/models/purchase_record_shipping_address_spec.rb
+++ spec/models/purchase_record_shipping_address_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe PurchaseRecordShippingAddress, type: :model do
   before do
     user = FactoryBot.create(:user)
     @purchase_record_shipping_address = PurchaseRecordShippingAddress.new(
+                                          token:"tok_abcdefghijk00000000000000000",
                                           postal_code: '123-4567', prefecture_id: 2, 
                                           city: 'A市', addresses: 'B区8-9', 
                                           building: 'Cビル10号室', phone_number: '09012345678', 
@@ -12,12 +13,12 @@ RSpec.describe PurchaseRecordShippingAddress, type: :model do
 
   describe '商品購入' do
     context '商品購入ができる場合' do
-      it 'postal_code・prefecture_id・city・addresses・phone_numberが適切な値で存在し、
+      it 'token・postal_code・prefecture_id・city・addresses・phone_numberが適切な値で存在し、
           buildingが存在する場合に商品購入ができる' do
         expect(@purchase_record_shipping_address).to be_valid
       end 
 
-      it 'postal_code・prefecture_id・city・addresses・phone_numberが適切な値で存在し、
+      it 'token・postal_code・prefecture_id・city・addresses・phone_numberが適切な値で存在し、
           buildingが空の場合でも商品購入ができる' do
         @purchase_record_shipping_address.building = ''
         expect(@purchase_record_shipping_address).to be_valid
@@ -25,6 +26,12 @@ RSpec.describe PurchaseRecordShippingAddress, type: :model do
     end
 
     context '商品購入ができない場合' do
+      it 'tokenが空では商品購入はできない' do
+        @purchase_record_shipping_address.token = ''
+        @purchase_record_shipping_address.valid?
+        expect(@purchase_record_shipping_address.errors.full_messages).to include("Token can't be blank")
+      end
+
       it 'postal_codeが空では商品購入はできない' do
         @purchase_record_shipping_address.postal_code = ''
         @purchase_record_shipping_address.valid?
